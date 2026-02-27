6a60250009ce5dc098b97e3bca8d12200cdb0f16
Ivan Bulakhov <core@kabarchonok.me>
2026-02-27 23:33:17 +0800

    Updated 404 template

---
+++ src/stories/views/NotFound.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf'
+  import NotFound from '../../views/NotFound.svelte'
+
+  const { Story } = defineMeta({
+    title: 'Views/NotFound',
+    component: NotFound,
+  })
+</script>
+
+<Story name='Base' />
--- src/views/NotFound.svelte
+++ src/views/NotFound.svelte
@@ -1,1 +1,26 @@
-404
+<script lang='ts'>
+  import Action from '../components/Action.svelte'
+  import State from '../components/State.svelte'
+  import { getUrl } from '../stores/router'
+</script>
+
+<div class='not-found'>
+  <State emoji='(·_·)?' title='Page not found'>
+    {#snippet actions()}
+      <Action class='not-found__action' href={getUrl('home')}>return to categories</Action>
+    {/snippet}
+  </State>
+</div>
+
+<style>
+  .not-found {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    height: 100%;
+
+    text-align: center;
+  }
+</style>
