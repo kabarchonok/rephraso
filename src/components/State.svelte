747ec2ec4799940a112315ffa5c51197edf9e0c2
Ivan Bulakhov <core@kabarchonok.me>
2026-04-05 19:09:07 +0800

    Updated app styles and layout

--- src/components/State.svelte
+++ src/components/State.svelte
@@ -5,8 +5,10 @@
 </script>
 
 <div class='state'>
-  <div class='state__emoji'>{emoji}</div>
-  <div class='state__title'>{title}</div>
+  <div class='state__content'>
+    <div class='state__emoji'>{emoji}</div>
+    <div class='state__title'>{title}</div>
+  </div>
 
   <div class='state__actions'>
     {@render actions()}
@@ -18,6 +20,13 @@
     display: flex;
     flex-direction: column;
     gap: 24px;
+    justify-content: center;
+
+    height: 100%;
+  }
+
+  .state__content {
+    margin-top: auto;
   }
 
   .state__emoji {
@@ -26,5 +35,9 @@
 
   .state__title {
     font-size: 32px;
+  }
+
+  .state__actions {
+    margin-top: auto;
   }
 </style>
